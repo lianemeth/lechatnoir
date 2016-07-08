de68fee4018d2a90aa507e7d371a5f01ea1acf13
Lucas Nemeth <lucas.nemeth@ericsson.com>
2016-07-08 17:50:57 +0100

    End Goodbye to the Hello

--- chat/chat_noir_server.erl
+++ chat/chat_noir_server.erl
@@ -8,8 +8,17 @@ start()->
 loop(Socket) ->
 	case gen_tcp:recv(Socket, 0) of
 		{ok, Data} ->
-			gen_tcp:send(Socket, "Hello world"),
-			loop(Socket);
+			io:format("Data: ~p~n", [binary_to_list(Data)]),
+			Message = binary_to_list(Data),
+			case Message of
+				"end\r\n" ->
+					gen_tcp:send(Socket, "Goodbye\n"),
+					gen_tcp:close(Socket),
+					ok;
+				_ ->
+					gen_tcp:send(Socket, "Hello world\n"),
+					loop(Socket)
+			end;
 		{error, closed} -> 
 			ok
 	end.
