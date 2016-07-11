c587d16e20df7a84a3a67b216e04c18ab03ad12f
Lucas Nemeth <lucas.nemeth@gmail.com>
2016-07-11 17:21:29 +0100

    add logig to args on tcp server & clients on chat

--- chat/tcp_server.erl
+++ chat/tcp_server.erl
@@ -3,18 +3,19 @@
 
 -export([init/1, code_change/3, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
 -export([accept_loop/1]).
--export([start/3]).
+-export([start/4]).
 
 -define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
 
 -record(server_state, {
 		port,
 		loop,
+		args,
 		ip=any,
 		lsocket=null}).
 
-start(Name, Port, Loop) ->
-	State = #server_state{port = Port, loop = Loop},
+start(Name, Port, Loop, Args) ->
+	State = #server_state{port = Port, loop = Loop, args=Args},
 	% register locally a server as Name
 	% Callback functions are found in ?MODULE (init, handle_call, handle_cast)
 	% State is passed to init
@@ -36,13 +37,13 @@ handle_cast({accepted, _Pid}, State=#server_state{})->
 handle_call({accepted, _Pid}, Caller, State=#server_state{})->
 	{stop, {unknown_call, Caller}, State}.
 
-accept_loop({Server, LSocket, {Module, Function}}) ->
+accept_loop({Server, LSocket, {Module, Function}, Args}) ->
 	{ok, Socket} = gen_tcp:accept(LSocket),
 	gen_server:cast(Server, {accepted, self()}),
-	Module:Function(Socket).
+	Module:Function(Socket, Args).
 
-accept(State=#server_state{lsocket=LSocket, loop=Loop}) ->
-	spawn(?MODULE, accept_loop, [{self(), LSocket, Loop}]),
+accept(State=#server_state{lsocket=LSocket, loop=Loop, args=Args}) ->
+	spawn(?MODULE, accept_loop, [{self(), LSocket, Loop, Args}]),
 	State.
 
 handle_info(_Msg, Library) -> {noreply, Library}.
