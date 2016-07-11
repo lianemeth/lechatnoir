f414d57c9cdbf372e82a771667c4df198ef56e35
Lucas Nemeth <lucas.nemeth@gmail.com>
2016-07-11 17:21:29 +0100

    add logig to args on tcp server & clients on chat

--- chat/chat_noir_server.erl
+++ chat/chat_noir_server.erl
@@ -1,9 +1,9 @@
 -module(chat_noir_server).
 
--export([start/0, loop/1, server_test/0]).
+-export([start/0, loop/2, server_test/0]).
 
 start()->
-	tcp_server:start(?MODULE, 8888, {?MODULE, loop}).
+	tcp_server:start(?MODULE, 8888, {?MODULE, loop}, []).
 
 do_recv(Socket) ->
 	case gen_tcp:recv(Socket, 0) of
@@ -16,13 +16,13 @@ do_recv(Socket) ->
 parse_message(SomeString) ->
 	{M, B} = lists:splitwith(fun(S) -> [S] =/= "{" end, SomeString).
 
-loop(Socket) ->
+loop(Socket, Clients) ->
 	Message = do_recv(Socket),
 	{Method, Body} = parse_message(Message),
 	case Method of
 		"NEWCLIENT" ->
 			gen_tcp:send(Socket, "New Client Arrived! \n"),
-			loop(Socket),
+			loop(Socket, Clients),
 			ok;
 		_ ->
 			gen_tcp:send(Socket, "Error\n"),
