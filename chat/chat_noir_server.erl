0380d2467890523e731b81850b54f078fc1acab7
Lucas Nemeth <lucas.nemeth@gmail.com>
2016-07-21 16:33:55 +0100

    tweak small stuff

--- chat/chat_noir_server.erl
+++ chat/chat_noir_server.erl
@@ -2,6 +2,12 @@
 
 -export([start/0, loop/2, server_test/0]).
 
+-record(client, {
+		name,
+		info,
+		socket
+	}).
+
 start()->
 	tcp_server:start(?MODULE, 8888, {?MODULE, loop}, []).
 
@@ -16,14 +22,29 @@ do_recv(Socket) ->
 parse_message(SomeString) ->
 	{M, B} = lists:splitwith(fun(S) -> [S] =/= "{" end, SomeString).
 
+parse_client(SomeString, Socket) ->
+	#client{name="Douglas", info="ada", socket=Socket}.
+
+parse_message(SomeString, Socket) ->
+	{10, 11}.
+
+public_message(User, Message, Clients)->
+	ok.
+
 loop(Socket, Clients) ->
 	Message = do_recv(Socket),
+	io:format("New Client ~p ", [Clients]),
 	{Method, Body} = parse_message(Message),
 	case Method of
 		"NEWCLIENT" ->
 			gen_tcp:send(Socket, "New Client Arrived! \n"),
-			loop(Socket, Clients),
-			ok;
+			NewClient = parse_client(Body, Socket),
+			io:format("New Client ~p ", [NewClient]),
+			loop(Socket, [NewClient | Clients]);
+		"PUBLIC" ->
+			User, ChatMsg = parse_message(Body, Socket),
+			public_message(User, ChatMsg, Clients),
+			loop(Socket, Clients);
 		_ ->
 			gen_tcp:send(Socket, "Error\n"),
 			gen_tcp:close(Socket)
