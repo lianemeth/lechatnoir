385f458d290c49c66e805a951cb5a30f929b428f
Lucas Nemeth <lucas.nemeth@gmail.com>
2016-10-17 14:46:01 +0100

    improvements in the parsing

--- chat/chat_noir_server.erl
+++ chat/chat_noir_server.erl
@@ -19,8 +19,10 @@ do_recv(Socket) ->
 			ok
 	end.
 
-parse_message(SomeString) ->
-	{M, B} = lists:splitwith(fun(S) -> [S] =/= "{" end, SomeString).
+get_message_type(SomeString) ->
+	[Type, B] = re:split(SomeString, "{", [{return, list}]),
+	Body = string:strip(B, right, $}),
+	{Type, Body}.
 
 parse_client(SomeString, Socket) ->
 	#client{name="Douglas", info="ada", socket=Socket}.
@@ -34,7 +36,7 @@ public_message(User, Message, Clients)->
 loop(Socket, Clients) ->
 	Message = do_recv(Socket),
 	io:format("New Client ~p ", [Clients]),
-	{Method, Body} = parse_message(Message),
+	{Method, Body} = get_message_type(Message),
 	case Method of
 		"NEWCLIENT" ->
 			gen_tcp:send(Socket, "New Client Arrived! \n"),
@@ -42,7 +44,7 @@ loop(Socket, Clients) ->
 			io:format("New Client ~p ", [NewClient]),
 			loop(Socket, [NewClient | Clients]);
 		"PUBLIC" ->
-			User, ChatMsg = parse_message(Body, Socket),
+			{User, ChatMsg} = parse_message(Body, Socket),
 			public_message(User, ChatMsg, Clients),
 			loop(Socket, Clients);
 		_ ->
@@ -51,4 +53,5 @@ loop(Socket, Clients) ->
 	end.
 
 server_test() ->
-	"NEWCLIENT", _ = parse_message("NEWCLIENT{NAME=douglas;}").
+	"NEWCLIENT", _ = get_message_type("NEWCLIENT{NAME=douglas;}"),
+        "PUBLIC", _ = get_message_type("PUBLIC{MSG=hello world;}").
